8dcc7a4b68c3943f6f8e051ef535aa46e709bb6d
jli09 <56615820+jli09@users.noreply.github.com>
2020-05-12 21:55:36 -0400

    up and down movement working

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 function init() {
     console.log('initialized');
 
-    document.addEventListener('keydown', (e) => {
-        console.log('key down: ', e.key);
-    })
-
-    document.addEventListener('keyup', (e) => {
-        console.log('key up: ', e.key);
-    })
-
     const stage = new createjs.Stage('main');
 
     const shape = new createjs.Shape();
-    shape.graphics.beginFill("red").drawRect(0, 0, 75, 75);
+
+    const shapeWidth = 75;
+    const shapeHeight = 75;
+
+    shape.graphics.beginFill("red").drawRect(0, 0, shapeWidth, shapeHeight);
     shape.x = 100;
     shape.y = 100;
 
+    console.log('shape: ', shape)
+
     stage.addChild(shape);
 
-    const tick = () => {
-        shape.x += 5;
-        if (shape.x > stage.canvas.width)
-            shape.x = 0;
+    stage.update();
+
+    // const tick = () => {
+    //     shape.x += 5;
+    //     if (shape.x > stage.canvas.width)
+    //         shape.x = 0;
         
-        stage.update();
-    }
+    //     stage.update();
+    // }
 
-    createjs.Ticker.framerate = 40;
-    createjs.Ticker.on('tick', tick);
+    // createjs.Ticker.framerate = 40;
+    // createjs.Ticker.on('tick', tick);
 
+    document.addEventListener('keydown', (e) => {
+        const { key } = e;
+        
+        console.log('key down: ', key);
 
+        if (key === 'ArrowUp' || key === 'w') {
+            if (shape.y > 0) {
+                shape.y -= 10;
+                stage.update();
+            }
+        }
+
+        if (key === 'ArrowDown' || key === 's') {
+            console.log(shape.y);
+
+          if (shape.y + shapeHeight < stage.canvas.height) {
+            shape.y += 10;
+            stage.update();
+          }
+        }
+        
+        
+    });
+
+    document.addEventListener('keyup', (e) => {
+      console.log('key up: ', e.key);
+    });
 }
