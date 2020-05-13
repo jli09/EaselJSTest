54d96053f59d9b6c50a23eae4fd56c876ea0d8d2
jli09 <56615820+jli09@users.noreply.github.com>
2020-05-12 21:08:44 -0400

    made rectangle

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function init() {
-    
+    console.log('initialized');
+
+    const stage = new createjs.Stage('main');
+
+    const shape = new createjs.Shape();
+    shape.graphics.beginFill("red").drawRect(0, 0, 75, 75);
+    shape.x = 100;
+    shape.y = 100;
+
+    stage.addChild(shape);
+
+    stage.update();
 }
